419c6ecefe66b4e8628335bb72bdea8051c0c834
daler <dalerr@niddk.nih.gov>
2014-05-08 09:34:13 -0400

    travis-ci python 3 config

--- .travis-install.sh
+++ .travis-install.sh
@@ -1,16 +1,22 @@
-# thanks https://github.com/rmcgibbo/mdtraj/blob/master/tools/ci/install.sh
-#
+#!/bin/bash
+
+# Thanks @rmcgibbo for ideas in
+# https://github.com/rmcgibbo/mdtraj/blob/master/tools/ci/install.sh
+
+# Be obnoxiously verbose to make debugging easier from Travis-CI logs.
+set -vx
 
 wget http://repo.continuum.io/miniconda/Miniconda3-3.4.2-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b
 
 export PATH=$HOME/miniconda3/bin:$PATH
-echo "${PATH}"
 
-# the $python var is set from the env:matrix entries in .travis.yml.
-conda create --yes -n ${python} python=${python} numpy matplotlib
+# The $python var is set from the env:matrix entries in .travis.yml.
+# We at least need pip.
+conda create --yes -n ${python} python=${python} pip numpy matplotlib
 source activate $python
 
-PYTHON_VERSION=`python -c 'import sys; print("%d.%d" % sys.version_info[:2])'`
+
+echo Using pip at: $(which pip)
 pip install sphinx sphinx_bootstrap_theme
 pip install .
